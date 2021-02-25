fc9ec626e66c35ee2e56347ab4f27c35354e78e0
Gagandeep Kaur <gagandeepkaur@Gagandeeps-MacBook-Air.local>
2021-02-24 22:14:23 -0500

    Travel form with validation

---
+++ reservation(1).js
@@ -0,0 +1,42 @@
+function validate() { 
+    var name = document.forms["RegForm"]["Name"]; 
+    var email = document.forms["RegForm"]["EMail"]; 
+    var phone = document.forms["RegForm"]["Telephone"]; 
+    var what = document.forms["RegForm"]["Adults"]; 
+    var address = document.forms["RegForm"]["Address"]; 
+
+    if (name.value == "") { 
+        window.alert("Please enter  your first name."); 
+        name.focus(); 
+        return false; 
+    } 
+
+    if (address.value == "") { 
+        window.alert("Please enter your address."); 
+        address.focus(); 
+        return false; 
+    } 
+
+    if (email.value == "") { 
+        window.alert( 
+          "Please enter a valid e-mail address."); 
+        email.focus(); 
+        return false; 
+    } 
+
+    if (phone.value == "") { 
+        window.alert( 
+          "Please enter your telephone number."); 
+        phone.focus(); 
+        return false; 
+    } 
+
+
+    if (what.selectedIndex < 1) { 
+        alert("Please write number of Adults"); 
+        what.focus(); 
+        return false; 
+    } 
+
+    return true; 
+} 
